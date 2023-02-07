0fd3fbf630eb937c68e3e31dbbe276359ce440bc
keitosuwahara <210103gi@yse-c.net>
2023-02-07 09:17:10 +0900

    pra packages

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module pra_go
 
 go 1.19
+
+require (
+	golang.org/x/net v0.5.0 // indirect
+	golang.org/x/text v0.6.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,52 +1,42 @@
 package main
 
 import (
+	"bufio"
 	f "fmt" // pythonでいうimport fmt as f をやっている感じ
+	"os"
 	// . "pra_go/animals"  パッケージ名をなくすこともできる
 )
 
-/*
-	type Person struct {
-	    firstName string
-	    age int
-	}
-*/
-var s = ""
-
 func init() {
-	s = "このinit関数はmain関数より先に実行される\nそれは初期化用の関数なため"
+	s := "このinit関数はmain関数より先に実行される\nそれは初期化用の関数なため"
+	f.Println(s)
 }
 
 func main() {
-	f.Println(s)
-	slices := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "m", "l", "n"}
-	slices[0] = "aaaaaaaa"
-	var slices2 []string
-	for index, slice := range slices {
-		f.Printf("[%d] ==> %v\n", index, slice)
-		slices2 = append(slices2, slice)
+scanner := bufio.NewScanner(os.Stdin)
+print("何か打ち込んで:::")
+var input string
+for scanner.Scan() {
+	input = scanner.Text()
+	break
 	}
-	println("____________________________________")
-	f.Println(slices2)
-	println("sum関数実行")
-	f.Println(sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20))
-	println("__________________________________")
-	println("連想配列作成")
-	suwaScore := map[string]int{"国語": 87, "数学": 10, "社会": 98, "英語": 90, "理科": 20}
 
-	f.Println(suwaScore["国語"])
+/*スキャンにエラーが出た時の処理*/
+
+if err := scanner.Err(); err != nil {
+	f.Println(os.Stderr, "読み込みエラー", err)
+}
+	f.Println("標準入力は"+input)
 }
 
+
+
 func sum(srcs ...int) int {
+	//可変長引数はようそがスライスで返される
 	num := 0
 	for _, src := range srcs {
 		num += src
 	}
 	return num
-
 }
 
-//これがコメント
-/*
-複数行
-*/
